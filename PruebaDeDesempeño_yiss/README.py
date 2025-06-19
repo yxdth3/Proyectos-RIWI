6af70114600db45252afc8aac63d4e2c717f2826
Yiss <128174682+yxdth3@users.noreply.github.com>
2025-06-18 23:02:09 -0500

    Prueba de Desempeño 1 - Módulo Python

---
+++ PruebaDeDesempeño_yiss/README.py
@@ -0,0 +1,15 @@
+
+#Makeup By Rose
+
+## La tienda de maquillaje By rose a crecido incrementalmente, por lo tanto, hemos requerido realizar un sistema 
+# de inventario dinámico, para que el control y la logística sea un poco más efectiva y más automatizada. Mejoradno así nuestro servicio, volvienéndolo
+# ágile y sencillo. 
+
+# DESCRIPCIÓN: EL sistema de inventario de las By rose, permite añadir productos recién llegados a stock, incluyendo nombre, precio y cantidades.
+# * Consultar productos, para conocer costos y disponibilidad en tiendas.
+# * Modificar el precio de un producto ya inventariado. 
+# * Eliminar productos que ya no tengan disponibilidad.
+# * Multiplicar el precio por la cantidad de cada producto y mostrar el total acumulado.
+
+# VERSIÓN: Python 3.10.12.
+#EDITOR DE TEXTO: Visual Studio Code.
